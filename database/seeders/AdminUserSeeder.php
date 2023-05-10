ec800a007323612e2b9d9084937db06334353088
br3-mah <nyeleti.bremah@gmail.com>
2023-05-10 17:20:28 +0200

    Admin User Seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,17 @@ class AdminUserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $user = User::create([
             'fname' => 'Shamba',
             'lname' => 'Adminstrator',
             'email' => 'admin@shamba.com',
             'password' => bcrypt('shamba.@123'),
         ]);
+
+        $user->ownedTeams()->save(Team::forceCreate([
+            'user_id' => $user->id,
+            'name' => explode(' ', $user->fname, 2)[0]."'s Hotel",
+            'personal_team' => true,
+        ]));
     }
 }
